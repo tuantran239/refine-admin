13c93fb245ec007a371a2becde41970157263a5f
tt239 <tuanphamtran99@gmail.com>
2024-11-21 15:37:16 +0700

    Setup deploy

--- deploy/scripts/set_up_env.sh
+++ deploy/scripts/set_up_env.sh
@@ -4,8 +4,9 @@ set -e
 
 echo "# Starting additional CI variables" >> .env
 
-
 echo $CI_DEPLOY_ENV_VARS | base64 -d >> .env
+
+echo $CI_DEPLOY_PRIVATE_KEY
 
 mkdir -p ~/.ssh
 ssh-keygen -t rsa -C "tuanphamtran99@gmail.com" -f ~/.ssh/id_rsa -P ""

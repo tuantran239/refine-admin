ebd97e85b8e398107dd736887d97ce545348817a
tt239 <tuanphamtran99@gmail.com>
2024-11-22 11:25:59 +0700

    Setup deploy

--- deploy/scripts/set_up_env.sh
+++ deploy/scripts/set_up_env.sh
@@ -9,11 +9,7 @@ echo $CI_DEPLOY_ENV_VARS | base64 -d >> .env
 echo $CI_DEPLOY_PRIVATE_KEY
 
 mkdir -p ~/.ssh
-ssh-keygen -t rsa -C "tuanphamtran99@gmail.com" -f ~/.ssh/id_rsa -P ""
-cd ~/.ssh
-echo $CI_DEPLOY_PRIVATE_KEY > id_rsa
-cat id_rsa
+cp $CI_DEPLOY_PRIVATE_KEY ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-cd ~    
+ssh-add ~/.ssh/id_rsa  
